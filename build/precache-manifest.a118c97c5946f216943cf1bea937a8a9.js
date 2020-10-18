76e69c1547653556cd85620d3c6d87782b43c137
Awong Nnange <awong.nnange@yahoo.com>
2020-10-18 19:41:42 +0200

    Updates

---
+++ build/precache-manifest.a118c97c5946f216943cf1bea937a8a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074a754aee4080f06529916c9357ebb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6b9c5c5fac280c0783",
+    "url": "/static/css/main.6a4c3bdf.chunk.css"
+  },
+  {
+    "revision": "d9d984d6210d98b7ab70",
+    "url": "/static/js/2.dd0073ba.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.dd0073ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6b9c5c5fac280c0783",
+    "url": "/static/js/main.db5bf834.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
